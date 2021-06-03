68bd096d3c240c7591a8d00e399c93707ab9510d
studentwhj8 <75083258+studentwhj8@users.noreply.github.com>
2021-06-03 12:24:43 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,38 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    //width=400, 400/2=200-70=130
+    //height = 400
+
+    title.position(displayWidth/2-70, 0);
+
+    this.input.position(displayWidth/2-70, displayHeight/2-80);
+    this.button.position(displayWidth/2, displayHeight/2);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2-70, displayHeight/4);
+    });
+
+  }
+}
